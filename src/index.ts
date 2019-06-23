1f84c0f84d71ab0e10705c2c88e493bdc0464848
Nik <nikibar4@gmail.com>
2019-06-23 14:40:35 +0300

    Deleted

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import 'phaser'
-import config from './config'
-
-let game = new Phaser.Game(config)
--- src/scenes/index.ts
+++
@@ -1,7 +0,0 @@
-import LoadScene from './LoadScene'
-import MenuScene from './MenuScene'
-import PlayScene from './PlayScene'
-
-export default {
-    LoadScene, MenuScene, PlayScene
-}
